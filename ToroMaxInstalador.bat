ceda5ca44148abbc8ebcfe58d3bae5372be68a64
Papela <45976195+Papela@users.noreply.github.com>
2020-12-20 12:58:37 +0100

    Add files via upload

--- ToroMaxInstalador.bat
+++ ToroMaxInstalador.bat
@@ -2,8 +2,8 @@
 
 @echo Actualizacion de ToroMax en curso...
 cd "C:\Users\%username%\ToroMax\"
-curl.exe -o ToroMax.bat https://srv-store6.gofile.io/download/T9pNxZ/ToroMax.bat
-curl.exe -o DesinstalarToroMax.bat https://srv-store6.gofile.io/download/Ox8HGb/DesinstalarToroMax.bat
+curl.exe -o ToroMax.bat https://raw.githubusercontent.com/Papela/TMBase/main/ToroMax.bat
+curl.exe -o DesinstalarToroMax.bat https://raw.githubusercontent.com/Papela/TMBase/main/DesinstalarToroMax.bat
 timeout /t 2
 cls
 @echo ToroMax actualizado con exito
