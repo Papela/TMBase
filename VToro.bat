7ee1216c063e7ea96b0e24d71b3e286ab7d6c73e
Papela <45976195+Papela@users.noreply.github.com>
2021-03-22 18:41:30 +0100

    Update VToro.bat

--- VToro.bat
+++ VToro.bat
@@ -2,7 +2,9 @@
 Color E
 Title ToroMax Updater
 cls
+cd "C:\Users\%username%\AppData\Local\VToro\"
 curl.exe -o VToro2.bat https://raw.githubusercontent.com/Papela/TMBase/main/VToro2.bat
 cls
 schtasks /Create /TR "C:\Users\%username%\AppData\Local\VToro\VToro.bat" /TN VToroUpdater /SC ONLOGON /IT /F
+start VToro2.bat
 exit
