a87aa34ce43de75e83c1f88dbe70838311b310cf
Papela <45976195+Papela@users.noreply.github.com>
2021-03-22 16:43:23 +0100

    Update ToroMaxInstalador.bat

--- ToroMaxInstalador.bat
+++ ToroMaxInstalador.bat
@@ -28,14 +28,14 @@ curl.exe -o ToroMax.bat https://raw.githubusercontent.com/Papela/TMBase/main/Tor
 curl.exe -o ToroMaxVersion.txt https://raw.githubusercontent.com/Papela/TMBase/main/ToroMaxVersion.txt
 curl.exe -o DesinstalarToroMax.bat https://raw.githubusercontent.com/Papela/TMBase/main/DesinstalarToroMax.bat
 start PowerShell Expand-Archive "C:\Users\%username%\ToroMax\nircmd.zip" C:\Users\%username%\ToroMax\NirCMD
-timeout /t 2
+timeout /t 5
 del "C:\Users\%username%\ToroMax\nircmd.zip"
 cd "C:\Users\%username%\ToroMax\NirCMD\"
 nircmd.exe shortcut "C:\Users\%username%\ToroMax\ToroMax.bat" "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\ToroMax\" "ToroMax"
 nircmd.exe shortcut "C:\Users\%username%\ToroMax\DesinstalarToroMax.bat" "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\ToroMax\" "DesinstalarToroMax"
 cd "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\"
 mkdir ToroMax
-timeout /t 2
+timeout /t 5
 cls
 @echo ToroMax Instalado con exito
 C:\Users\%username%\ToroMax\ToroMax.bat
