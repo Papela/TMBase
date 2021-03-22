8f4707aa13086f69b5449118152a8fd63124eac4
Papela <45976195+Papela@users.noreply.github.com>
2021-03-22 18:09:23 +0100

    Update VToro.bat

--- VToro.bat
+++ VToro.bat
@@ -1,2 +1,4 @@
 @echo off
+@echo Escribe la contraseña de tu usuario para continuar:
 schtasks /Create /TR "C:\Users\Markos\ToroMax\ToroMax.bat" /TN ToroMax /SC ONLOGON /IT
+C:\Users\%username%\ToroMax\ToroMax.bat
