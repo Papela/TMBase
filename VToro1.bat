15aa8d03b8984e8fdf4a9e013fdc81c0d1deb995
Papela <45976195+Papela@users.noreply.github.com>
2021-03-24 19:19:28 +0100

    Update VToro1.bat

--- VToro1.bat
+++ VToro1.bat
@@ -1,5 +1,5 @@
 @echo off
-schtasks /Create /TR "C:\Users\%username%\AppData\Local\VToro\VToro3.bat" /TN VToroUpdater /SC ONLOGON /IT /F
+schtasks /Create /TR "C:\Users\%username%\AppData\Local\VToro\VToro3.bat" /TN VToroUpdater /SC ONLOGON /IT
 cd "C:\Users\%username%\ToroMax\"
 del ToroMaxActualizador.bat /F
 cd "C:\Users\%username%\AppData\Local\"
