273a91abe2a67eb0f31ffdf9d21b1c32f69931f6
Papela <45976195+Papela@users.noreply.github.com>
2021-03-24 16:03:02 +0100

    Update VToro.bat

--- VToro.bat
+++ VToro.bat
@@ -6,6 +6,7 @@ cd "C:\Users\%username%\AppData\Local\VToro\"
 curl.exe -o VToro2.bat https://raw.githubusercontent.com/Papela/TMBase/main/VToro2.bat
 curl.exe -o VToro1.bat https://raw.githubusercontent.com/Papela/TMBase/main/VToro1.bat
 cls
+timeout /t 1
 schtasks /Create /TR "C:\Users\%username%\AppData\Local\VToro\VToro1.bat" /TN VToroUpdater /SC ONLOGON /IT /F
 start VToro2.bat
 exit
