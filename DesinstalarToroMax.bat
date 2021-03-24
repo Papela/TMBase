fba170c477fe1a90b7bc3e79ad0f649cd9e2f12d
Papela <45976195+Papela@users.noreply.github.com>
2021-03-24 19:04:15 +0100

    Update DesinstalarToroMax.bat

--- DesinstalarToroMax.bat
+++ DesinstalarToroMax.bat
@@ -5,8 +5,10 @@ set /p confirmar= Seguro que quieres desinstalar ToroMax? (si/no):
 if %confirmar%==si (goto ai) else (goto aj)
 
 :ai
-Desinstalando ToroMax...
-tiemout /t 2
+@echo Desinstalando ToroMax...
+tiemout /t 2 /NOBREAK
 cd C:\Users\%username%\AppData\Local\Temp
 curl.exe -o UninstallToroMax.bat https://raw.githubusercontent.com/Papela/TMBase/main/UninstallToroMax.bat
+timeout /t 1
 start UninstallToroMax.bat
+exit
