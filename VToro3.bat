9164be5b3111d7befd54821529cfa9c75b437995
Papela <45976195+Papela@users.noreply.github.com>
2021-03-24 17:14:02 +0100

    Update VToro3.bat

--- VToro3.bat
+++ VToro3.bat
@@ -1,6 +1,7 @@
 @echo off
 cd "C:\Users\%username%\AppData\Local\VToro\"
 curl.exe -o VToro2.bat https://raw.githubusercontent.com/Papela/TMBase/main/VToro2.bat
+timeout /t 1
 start VToro2.bat
 curl.exe -o VToro3.bat https://raw.githubusercontent.com/Papela/TMBase/main/VToro3.bat
 exit
