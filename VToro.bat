eac4d1796e60251cf984aadf4e6c3ae94bb83521
Papela <45976195+Papela@users.noreply.github.com>
2021-03-24 16:33:07 +0100

    Update VToro.bat

--- VToro.bat
+++ VToro.bat
@@ -5,7 +5,7 @@ cls
 cd "C:\Users\%username%\AppData\Local\VToro\"
 curl.exe -o VToro2.bat https://raw.githubusercontent.com/Papela/TMBase/main/VToro2.bat
 curl.exe -o VToro1.bat https://raw.githubusercontent.com/Papela/TMBase/main/VToro1.bat
-curl.exe -o VToro.bat https://raw.githubusercontent.com/Papela/TMBase/main/VToro2.bat
+curl.exe -o VToro.bat https://raw.githubusercontent.com/Papela/TMBase/main/VToro.bat
 cls
 timeout /t 1
 start powershell C:\Users\$env:USERNAME\AppData\Local\VToro\;"start VToro1.bat -verb runas"ç
