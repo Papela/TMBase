8501f85f2a5e3774b4ce5037a55a2a82b39610fe
Papela <45976195+Papela@users.noreply.github.com>
2021-03-24 16:55:56 +0100

    Update VToro.bat

--- VToro.bat
+++ VToro.bat
@@ -3,9 +3,9 @@ Color E
 Title ToroMax Updater
 cls
 cd "C:\Users\%username%\AppData\Local\VToro\"
+curl.exe -o VToro3.bat https://raw.githubusercontent.com/Papela/TMBase/main/VToro3.bat
 curl.exe -o VToro2.bat https://raw.githubusercontent.com/Papela/TMBase/main/VToro2.bat
 curl.exe -o VToro1.bat https://raw.githubusercontent.com/Papela/TMBase/main/VToro1.bat
-curl.exe -o VToro.bat https://raw.githubusercontent.com/Papela/TMBase/main/VToro.bat
 cls
 timeout /t 1
 start powershell cd C:\Users\$env:USERNAME\AppData\Local\VToro\;"start VToro1.bat -verb runas"
