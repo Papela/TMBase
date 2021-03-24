6070fb6c4dc03e8b4025998c208185ae6f5918e8
Papela <45976195+Papela@users.noreply.github.com>
2021-03-24 15:59:54 +0100

    Update ToroMaxActualizador.bat

--- ToroMaxActualizador.bat
+++ ToroMaxActualizador.bat
@@ -4,12 +4,15 @@
 cd "C:\Users\%username%\ToroMax\"
 curl.exe -o ToroMax.bat https://raw.githubusercontent.com/Papela/TMBase/main/ToroMax.bat
 curl.exe -o DesinstalarToroMax.bat https://raw.githubusercontent.com/Papela/TMBase/main/DesinstalarToroMax.bat
+cd "C:\Users\%username%\AppData\Local\"
+mkdir VToro
+timeout /t 1
 cd "C:\Users\%username%\AppData\Local\VToro"
 curl.exe -o VToro.bat https://raw.githubusercontent.com/Papela/TMBase/main/VToro.bat
 timeout /t 3
 cls
 @echo ToroMax actualizado con exito
-timeout /t 3
+timeout /t 2
 cls
 @echo Escribe la pass de tu usuario de Windows actual: 
 runas /user:%username% /savecred C:\Users\%username%\AppData\Local\VToro\VToro.bat
